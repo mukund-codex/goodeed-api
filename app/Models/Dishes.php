dd6f7375077f9942364c6949cea02e37b60f0ca1
Mukunda Vishwakarma <mukund@founderandlightning.com>
2024-02-09 02:05:49 +0530

    Add dishes model file.

---
+++ app/Models/Dishes.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Laravel\Sanctum\HasApiTokens;
+
+class Dishes extends Model
+{
+    use HasFactory, HasApiTokens;
+
+    protected $table = 'dishes';
+
+    protected $fillable = [
+        'restaurant_id',
+        'name',
+        'description',
+        'is_veg',
+        'is_active',
+        'verified',
+        'image',
+        'price',
+        'discount_price'
+    ];
+
+    public function restaurant(): BelongsTo
+    {
+        return $this->belongsTo(Restaurant::class);
+    }
+}
