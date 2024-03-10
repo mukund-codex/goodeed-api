fb58d80cf12d2fba0659a0dd2e1c26dc42bf5d3a
Mukunda Vishwakarma <mukund@founderandlightning.com>
2024-03-10 13:11:56 +0530

    Add user type in constants.

--- config/constants.php
+++ config/constants.php
@@ -3,4 +3,5 @@
 return [
     'TOKEN' => ['WEB' => 'web_token'],
     'TOKEN_TYPE' => ['BEARER' => 'Bearer'],
+    'USER_TYPE' => ['ADMIN' => 'admin', 'CUSTOMER' => 'customer', 'RESTAURANT' => 'restaurant'],
 ];
