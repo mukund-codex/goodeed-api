0b9f85341f2f9b23fccfa1785a791697110fc4df
Mukunda Vishwakarma <mukund@founderandlightning.com>
2024-03-10 22:27:47 +0530

    Remove 2 unused packages from Restaurant controller.

--- app/Http/Controllers/v1/RestaurantController.php
+++ app/Http/Controllers/v1/RestaurantController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\v1;
 
+use App\Exceptions\ApiException;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Restaurant\AddRestaurantRequest;
 use App\Http\Requests\Restaurant\UpdateRestaurantRequest;
@@ -9,6 +10,7 @@ use App\Http\Traits\ApiResponseTrait;
 use App\Repositories\Restaurant\RestaurantRespository;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class RestaurantController extends Controller
@@ -32,8 +34,17 @@ class RestaurantController extends Controller
         );
     }
 
+    /**
+     */
     public function create(AddRestaurantRequest $request): JsonResponse
     {
+        if (Auth::user()->type !== config('constants.USER_TYPE.RESTAURANT')) {
+            return $this->showFailureRequest(
+                [],
+              __('messages.restaurant.create.failed'),
+              Response::HTTP_FORBIDDEN
+            );
+        }
         $restaurant = $this->restaurant->create($request->validated());
         return $this->showSuccessResponse(
           $restaurant,
@@ -44,6 +55,13 @@ class RestaurantController extends Controller
 
     public function update(UpdateRestaurantRequest $request): JsonResponse
     {
+        if (Auth::user()->type !== config('constants.USER_TYPE.RESTAURANT')) {
+            return $this->showFailureRequest(
+                [],
+                __('messages.restaurant.create.failed'),
+                Response::HTTP_FORBIDDEN
+            );
+        }
         $restaurant = $this->restaurant->update($request->validated());
         return $this->showSuccessResponse(
           $restaurant,
