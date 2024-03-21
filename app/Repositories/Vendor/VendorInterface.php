7c3762691efae707582045ace8dfd1c7625c0b34
Mukunda Vishwakarma <mukund@founderandlightning.com>
2024-03-21 17:57:13 +0530

    Add create, LoginHandler and verify functions in Vendor Interface.

--- app/Repositories/Vendor/VendorInterface.php
+++ app/Repositories/Vendor/VendorInterface.php
@@ -3,11 +3,18 @@
 namespace App\Repositories\Vendor;
 
 use App\Models\Vendor;
+use Illuminate\Http\RedirectResponse;
 
 interface VendorInterface
 {
     public function login(array $data): Vendor;
 
     public function verifyOtp(array $data): Vendor;
+
+    public function create(array $data): RedirectResponse;
+
+    public function loginHandler(array $data): array;
+
+    public function verify(string $token): RedirectResponse;
 
 }
