2bb77fd3d218019b6923b6b4dddba2d31f3eb528
Mukunda Vishwakarma <mukund@founderandlightning.com>
2024-03-21 17:47:41 +0530

    Add Admin Model.

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,8 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $guard = 'admin'; // 'admin' is the name of the guard in config/auth.php
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'mobile_number',
+        'image',
+        'status',
+        'is_email_verified',
+        'email_verified_at',
+        'is_mobile_verified',
+        'is_active',
+        'is_deleted',
+        'is_blocked',
+        'type'
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
 }
