dcb8f1f646e6fac3c72183001f58230b9a008e12
Mukunda Vishwakarma <mukund@founderandlightning.com>
2024-03-21 17:55:50 +0530

    Add vendor model.

--- app/Models/Vendor.php
+++ app/Models/Vendor.php
@@ -5,11 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class Vendor extends Model
+class Vendor extends Authenticatable
 {
-    use HasFactory, HasApiTokens;
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $guard = 'vendor'; // 'vendor' is the name of the guard in config/auth.php
 
     protected $fillable = [
         'name',
