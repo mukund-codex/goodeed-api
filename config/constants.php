76b88dbfc5113c404348cafadfa1c7da4e909b91
Mukunda Vishwakarma <mukund@founderandlightning.com>
2024-03-21 17:51:14 +0530

    Change in restaurant type to vendor in constants.

--- config/constants.php
+++ config/constants.php
@@ -3,6 +3,6 @@
 return [
     'TOKEN' => ['WEB' => 'web_token'],
     'TOKEN_TYPE' => ['BEARER' => 'Bearer'],
-    'USER_TYPE' => ['ADMIN' => 'admin', 'CUSTOMER' => 'customer', 'RESTAURANT' => 'restaurant'],
+    'USER_TYPE' => ['ADMIN' => 'admin', 'CUSTOMER' => 'customer', 'VENDOR' => 'vendor', 'DELIVERY' => 'delivery'],
     'STATUS' => ['ACTIVE' => 'active', 'INACTIVE' => 'inactive', 'BLOCKED' => 'blocked'],
 ];
