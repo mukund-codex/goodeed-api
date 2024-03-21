e8c45d77a08d75a14994ecda374bd49e66c30624
Mukunda Vishwakarma <mukund@founderandlightning.com>
2024-03-21 17:54:36 +0530

    Add register success blade file for vendors.

---
+++ resources/views/vendors/auth/register-success.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register Success</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+        }
+        .reg-success {
+            width: 100%;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+        }
+        .reg-success span {
+            font-size: 5rem;
+            color: #4caf50;
+        }
+        .reg-success p {
+            font-size: 1.5rem;
+            color: #7ff785;
+            text-align: center;
+            font-weight: bold;
+        }
+</style>
+</head>
+<body>
+    <div class="reg-success">
+        <span>&check;</span>
+        <p>Thank you for registering with us. We have sent you an activation link. Please check your email and click on the link to verify your account.</p>
+    </div>
+</body>
+</html>
