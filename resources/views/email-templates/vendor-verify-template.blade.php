617b403e2a3766ec9acbe850903636fee0888290
Mukunda Vishwakarma <mukund@founderandlightning.com>
2024-03-21 17:56:32 +0530

    Add email template for vendor email verification mail.

---
+++ resources/views/email-templates/vendor-verify-template.blade.php
@@ -0,0 +1,10 @@
+<p>
+    Dear {{ $name }},
+</p>
+
+<p>
+    Thank you for registering with us. Please click on the link below to verify your email address.
+    <br>
+    Click here to verify your email address: <br>
+    <a href="{{ $actionLink }}"> Verify Account!</a>
+</p>
