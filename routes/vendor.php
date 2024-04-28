562f9f2cd1eb6c8b90f1c97076b5040d5256ac09
Mukunda Vishwakarma <mukund@founderandlightning.com>
2024-04-28 21:08:36 +0530

    Change in vendor dashboard routes.

--- routes/vendor.php
+++ routes/vendor.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\vendors\DishesController;
+use App\Http\Controllers\vendors\RestaurantController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\vendors\VendorController;
 
@@ -17,6 +19,26 @@ Route::prefix('vendors')->name('vendors.')->group(function () {
     Route::middleware([])->group(function () {
         Route::view('/dashboard', 'vendors.dashboard')->name('dashboard');
         Route::post('/logout-handler', [VendorController::class, 'logoutHandler'])->name('logout-handler');
+
+        // Restaurants Routes
+        Route::get('/restaurants', [RestaurantController::class, 'index'])->name('restaurants');
+        Route::view('/restaurant/add', 'vendors.restaurants.add')->name('restaurant.add');
+        Route::post('/restaurants/create', [RestaurantController::class, 'store'])->name('restaurants.create');
+        Route::get('/restaurants/edit/{id}', [RestaurantController::class, 'edit'])->name('restaurants.edit');
+        Route::post('/restaurants/update/{id}', [RestaurantController::class, 'update'])->name('restaurants.update');
+        Route::get('/restaurants/delete/{id}', [RestaurantController::class, 'delete'])->name('restaurants.delete');
+
+
+        // Dishes Routes
+        Route::get('/dishes', [DishesController::class, 'index'])->name('dishes');
+        Route::get('/dishes/add', [DishesController::class, 'create'])->name('dishes.add');
+        Route::post('/dishes/create', [DishesController::class, 'store'])->name('dishes.create');
+        Route::get('/dishes/edit/{id}', [DishesController::class, 'edit'])->name('dishes.edit');
+        Route::post('/dishes/update/{id}', [DishesController::class, 'update'])->name('dishes.update');
+        Route::get('/dishes/delete/{id}', [DishesController::class, 'delete'])->name('dishes.delete');
+
+        Route::post('/get-subcategories', [DishesController::class, 'getSubcategories'])->name('get-subcategories');
+
     });
 
 });
